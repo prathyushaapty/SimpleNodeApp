db4cbfa8374ddc57a675e662975d83bfc30851ee
prathyusha <prathyusha.bandela@letznav.com>
2017-09-22 16:21:33 +0530

    added express code

--- server.js
+++ server.js
@@ -4,17 +4,25 @@ var http = require('http');
 const express = require('express');
 const app = express()
 
-//create a server instance and binding at port 8081
-http.createServer(function(request,response){
-	//send the http header
-	//Http status:200 OK
-	//Content Type : text/plain
-	response.writeHead(200,{'Content-Type':'text/plain'});
+app.get('/',function(request,response){
+	response.send('Hello world ##############');
+});
 
-	//send the response body
-	response.end('Server running successfully');
+app.listen(process.env.PORT||8099,function(){
+	console.log("app listening on the port 8099");
+});
+
+// //create a server instance and binding at port 8081
+// http.createServer(function(request,response){
+// 	//send the http header
+// 	//Http status:200 OK
+// 	//Content Type : text/plain
+// 	response.writeHead(200,{'Content-Type':'text/plain'});
+
+// 	//send the response body
+// 	response.end('Server running successfully');
 	
-}).listen(process.env.PORT || 8099);
+// }).listen(process.env.PORT || 8099);
 
-console.log("server running at localhost:8099");
+// console.log("server running at localhost:8099");
 
